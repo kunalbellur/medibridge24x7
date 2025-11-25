d8636bd566aa6df3c3bf472525564e9b98bb980c
kunalbellur <kunalbellur@gmail.com>
2025-11-25 17:29:10 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/layouts/**/*.{js,ts,jsx,tsx}",
+    "./medibridge-extra/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
